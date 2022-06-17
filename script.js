eb9391b74cb4aa73c6ff66f820affa6dfdc32963
janae.wel <janae.wel@gmail.com>
2022-06-17 19:13:05 -0400

    question randomization done

--- script.js
+++ script.js
@@ -1,78 +1,46 @@
 // VARIABLES
 // getting vars
 var mainSectionEl = document.querySelector("#main"); // OFFICE HOURS: Is getting an element by its class appropriate? Should it always be by its id?
-var answersBox = document.querySelector("#answer");
 var time = document.querySelector(".time");
 // created elements
 var h1El = document.createElement("h1");
 var pEl = document.createElement("p");
-var startBtnEl = document.createElement("button");
-var answer1 = document.createElement("button");
-var answer2 = document.createElement("button");
-var answer3 = document.createElement("button");
-var rightAnswer = document.createElement("button");
+var newBtn = document.createElement("button");
+var startBtnEl = newBtn;
+var answer1 = newBtn;
+var answer2 = newBtn;
+var answer3 = newBtn;
+var rightAnswer = newBtn;
 var feedback = document.createElement("h2");
 // question / answer objects
 var q1 = {
     question: "What do you think about Question 1?",
-    answers: {
-        answer1: "Question 1 Answers",
-        answer2: "Question 1 Answers",
-        answer3: "Question 1 Answers",
-        rightAnswer: "Question 1 Right Answer"
-    }
+    answers: ["Question 1 Answers", "Question 1 Answers", "Question 1 Answers", "Right Answer"]
 }
 var q2 = {
     question: "What do you think about Question 2?",
-    answers: {
-        answer1: "Question 2 Answers",
-        answer2: "Question 2 Answers",
-        answer3: "Question 2 Answers",
-        rightAnswer: "Question 2 Right Answer"
-    }
+    answers: ["Question 2 Answers", "Question 2 Answers", "Question 2 Answers", "Right Answer"]
 }
 var q3 = {
     question: "What do you think about Question 3?",
-    answers: {
-        answer1: "Question 3 Answers",
-        answer2: "Question 3 Answers",
-        answer3: "Question 3 Answers",
-        rightAnswer: "Question 3 Right Answer"
-    }
+    answers: ["Question 3 Answers", "Question 3 Answers", "Question 3 Answers", "Right Answer"]
 }
 var q4 = {
     question: "What do you think about Question 4?",
-    answers: {
-        answer1: "Question 4 Answers",
-        answer2: "Question 4 Answers",
-        answer3: "Question 4 Answers",
-        rightAnswer: "Question 4 Right Answer"
-    }
+    answers: ["Question 4 Answers", "Question 4 Answers", "Question 4 Answers", "Right Answer"]
 }
 var q5 = {
     question: "What do you think about Question 5?",
-    answers: {
-        answer1: "Question 5 Answers",
-        answer2: "Question 5 Answers",
-        answer3: "Question 5 Answers",
-        rightAnswer: "Question 5 Right Answer"
-    }
+    answers: ["Question 5 Answers", "Question 5 Answers", "Question 5 Answers", "Right Answer"]
 }
 var q6 = {
     question: "What do you think about Question 6?",
-    answers: {
-        answer1: "Question 6 Answers",
-        answer2: "Question 6 Answers",
-        answer3: "Question 6 Answers",
-        rightAnswer: "Question 6 Right Answer"
-    }
+    answers: ["Question 6 Answers", "Question  Answers", "Question 6 Answers", "Right Answer"]
 }
-// questions/answers arrays
+// arrays
 var questionsArr = [q1, q2, q3, q4, q5, q6];
-var answersArr = [answer1, answer2, answer3, rightAnswer];
-// page objects
 var homePage = [h1El, pEl, startBtnEl];
-var newScreen = [h1El, answersArr, feedback];
+var finishedQuestions = [];
 
 // HOME SCREEN
 // setting main
@@ -99,41 +67,80 @@ mainSectionEl.appendChild(startBtnEl);
 
 
 // START QUIZ FUNCTION
-function startQuiz(){
-    console.log("QUIZ STARTED");
+function clearHomePage(){
     // Remove home page
     for(var i = 1; i < homePage.length; i++){
         homePage[i].remove();
     }
-    printQuestion();
 };
+
+function createNewPage(){
+    // Store random number so that it doesn't randomize whenever mentioned
+    var randomQuestionNum = randomize(0, questionsArr.length);
+    console.log("Array value", randomQuestionNum);
+    // Stores value of the current question object
+    var currentQuestion = questionsArr[randomQuestionNum];
+
+    // Change h1 to a random question
+    h1El.textContent =  currentQuestion.question;
+    // Create a current question "answers value var"
+    var currentAnswer = currentQuestion.answers;
+
+    // Create a ul in mainSectionEl
+    var answersListEl = document.createElement("ul");
+    answersListEl.setAttribute("id", "answers");
+    mainSectionEl.appendChild(answersListEl);
+    // Randomize current answer array
+    shuffle(currentAnswer);
+    // Create an li for each item in currentAnswer array
+    // NOTE: forEach() reference from: https://gomakethings.com/two-more-ways-to-create-html-from-an-array-of-data-with-vanilla-js/
+    currentAnswer.forEach(function(currentAnswer){
+        var answer = document.createElement("li");
+        answer.innerHTML = "<button id='answer-btn' class='btn'>" + currentAnswer + "</button>";
+        answersListEl.appendChild(answer);
+    });
+}
 
 // RANDOMIZER
 function randomize(min, max){
     // Creating randomizer function
-    var randomValue = Math.floor((Math.random() * max + 1 - min) + min);
+    var randomValue = Math.floor((Math.random() * max - min) + min);
     return randomValue;
 }
 
-function printQuestion(){
-    // Store random number so that it can be tracked throughout function
-    var randomQuestionNum = randomize(0, questionsArr.length);
-    console.log("Array value", randomQuestionNum);
-
-    // Change h1 to random question
-    h1El.textContent =  questionsArr[randomQuestionNum].question;
-    
-    // Print a random object's question
-    for(var i = 0; i < questionsArr[randomQuestionNum].answers; i++){
-        answersBox.appendChild(questionsArr[randomQuestionNum].answers[i]);
+// INDEX SHUFFLER (source: https://www.youtube.com/watch?v=tLxBwSL3lPQ)
+function shuffle(arr){
+    // In one line of code, create three vars that store the i's length, randomized j and the random index position
+    var i = arr.length, j, indexPosition;
+    // Starting at arr.length (ie. "i") and incremening DOWN until 1...
+    while(--i > 0){
+        // Assign j a random value between arr's index length + 1 and 0
+        j = randomize(0, i+1);
+        // indexPosition will placehold the randomly selected index value
+        indexPosition = arr[j];
+        // random index placement = incremental index placement
+        arr[j] = arr[i];
+        // incremental index placement will claim indexPosition's value
+        arr[i] = indexPosition;
     }
-    
-    // Print out the questions answers in a random order (NOT RANDOMIZED YET)
-    for(var i = 0; i < answersArr.length; i++){
-        answersArr[i].className = "btn";
-        answersArr[i].setAttribute("id", "answer-btn");
-        answersArr[i].innerHTML = "<p>" + answersArr[i] + "<p>";
-        answersBox.appendChild(answersArr[i]);
-    }
-    // Remove question objects that have already been asked, by pushing them to a different array. (Might hypothetically be useful to keep questions and values for feature updates)
+    // Return shuffled index
+    return arr;
 }
+
+function startQuiz(){
+    clearHomePage();
+    createNewPage();
+    startTimer();
+    
+    
+
+    finishedQuestionsHandler();
+}
+
+function finishedQuestionsHandler(){
+    // Remove question objects that have already been asked, by pushing them to a different array. (Might hypothetically be useful to keep questions and values for feature updates)
+    currentQuestion.push(finishedQuestions);
+    console.log(finishedQuestions);
+    delete currentQuestion;
+}
+    
